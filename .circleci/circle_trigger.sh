3e6391d39393008bbb45a5ea14acd80a440fc017
tripheo0412 <tripheo0412@gmail.com>
2020-01-15 10:15:22 +0200

    script test

--- .circleci/circle_trigger.sh
+++ .circleci/circle_trigger.sh
@@ -11,7 +11,7 @@ echo ${CIRCLE_BRANCH}
 ## 1. Commit SHA of last CI build
 ############################################
 LAST_COMPLETED_BUILD_URL="${CIRCLE_API}/v1.1/project/${REPOSITORY_TYPE}/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}/tree/${CIRCLE_BRANCH}?filter=completed&limit=100&shallow=true"
-echo $(curl -Ss -u "${CIRCLE_API}/v1.1/project/${REPOSITORY_TYPE}/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}/tree/${CIRCLE_BRANCH}?circle-token=${CIRCLE_TOKEN}&filter=completed&limit=100&shallow=true")
+echo LAST_COMPLETED_BUILD_URL
 LAST_COMPLETED_BUILD_SHA=`curl -Ss -u "${CIRCLE_TOKEN}:" "${LAST_COMPLETED_BUILD_URL}" | jq -r 'map(select(.status == "success") | select(.workflows.workflow_name != "ci")) | .[0]["vcs_revision"]'`
 
 if  [[ ${LAST_COMPLETED_BUILD_SHA} == "null" ]]; then
