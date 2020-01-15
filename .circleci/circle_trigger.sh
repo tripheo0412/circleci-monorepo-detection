de0f8f25cd1b91f3150e9a40d3ecad9793ae1996
tripheo0412 <tripheo0412@gmail.com>
2020-01-15 10:23:17 +0200

    script test

--- .circleci/circle_trigger.sh
+++ .circleci/circle_trigger.sh
@@ -11,8 +11,8 @@ echo ${CIRCLE_BRANCH}
 ## 1. Commit SHA of last CI build
 ############################################
 LAST_COMPLETED_BUILD_URL="${CIRCLE_API}/v1.1/project/${REPOSITORY_TYPE}/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}/tree/${CIRCLE_BRANCH}?filter=completed&limit=100&shallow=true"
-echo $(curl -Ss ${LAST_COMPLETED_BUILD_URL})
-LAST_COMPLETED_BUILD_SHA=`curl -Ss "${LAST_COMPLETED_BUILD_URL}" | jq -r 'map(select(.status == "success") | select(.workflows.workflow_name != "ci")) | .[0]["vcs_revision"]'`
+
+LAST_COMPLETED_BUILD_SHA=`curl -Ss ${LAST_COMPLETED_BUILD_URL} | jq -r 'map(select(.status == "success") | select(.workflows.workflow_name != "ci")) | .[0]["vcs_revision"]'`
 echo ${LAST_COMPLETED_BUILD_SHA}
 if  [[ ${LAST_COMPLETED_BUILD_SHA} == "null" ]]; then
   echo -e "\e[93mThere are no completed CI builds in branch ${CIRCLE_BRANCH}.\e[0m"
